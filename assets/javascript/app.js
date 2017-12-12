0f30e22b64ee05b2074a022b66ca8546bea75ad3
Papa Deere <papadeere5150@gmail.com>
2017-12-12 13:55:54 -0700

    /assets
    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,36 @@
-// not sure if i need this
-$(document).ready(function(){
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD1NaPqtCKzVskbP3AYgLmCGbYqhtnWVfw",
-    authDomain: "traintime-144a3.firebaseapp.com",
-    databaseURL: "https://traintime-144a3.firebaseio.com",
-    projectId: "traintime-144a3",
-    storageBucket: "",
-    messagingSenderId: "567095252165"
-  };
-  firebase.initializeApp(config);
+           // Initialize Firebase
+            var config = {
+                apiKey: "AIzaSyD1NaPqtCKzVskbP3AYgLmCGbYqhtnWVfw",
+                authDomain: "traintime-144a3.firebaseapp.com",
+                databaseURL: "https://traintime-144a3.firebaseio.com",
+                projectId: "traintime-144a3",
+                storageBucket: "traintime-144a3.appspot.com/",
+                messagingSenderId: "567095252165"
+            };
+            firebase.initializeApp(config);
 
-  var database = firebase.database();
+            var database = firebase.database();
+            // Button for adding trains
+            $("#addTrainBtn").on("click", function(){
+                //Set variables from user input
+                var trainName = $("#trainNameInput").val().trim();
+                var destination = $("#destInput").val().trim();
+                var firstTrainTime = moment($("#timeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+                var frequency = $("#frequencyInput").val().trim();
+
+                //create new object and push to the database
+                var newTrain = {
+                    name: trainName,
+                    dest: destination,
+                    first: firstTrainTime,
+                    freq: frequency,
+                }
+                database.ref().push(newTrain);
+
+                console.log(newTrain.name); console.log(newTrain.dest); console.log(first); console.log(freq);
+
+                //clear out the form fields
+                $("#trainName").val(""); $("#destination").val(""); $("#firstTrainTime").val(""); $("#frequency").val("");
+
+                return false;
+            })
